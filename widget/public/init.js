15801e11abab5c8129a4b7b4f474adc6e03bad56
adejota <ademir.diamentejunior@gmail.com>
2021-02-22 11:04:54 -0300

    import FingerprintJS

--- widget/public/init.js
+++ widget/public/init.js
@@ -1,8 +1,10 @@
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
 function init (apiKey) {
 
   async function handleLoadWidget () {
     const page = `${window.location.origin}${window.location.pathname}`
-    const fp = await window.fingerprintJs.load()
+    const fp = await FingerprintJS.load()
     const fingerprint = await fp.get()
 
     const WIDGET_URL = `https://adejota-feedbacker-widget.netlify.app?api_key=${apiKey}&page=${page}&fingerprint=${fingerprint.visitorId}`
